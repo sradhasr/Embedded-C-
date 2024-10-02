61ac721870ceb82c826df481867fdc62eee39d94
Sradha <sradhabisangisr@gmail.com>
2024-10-02 21:54:48 +0530

    date and author name added

--- assignment 1/evaluate eqn.c
+++ assignment 1/evaluate eqn.c
@@ -1,18 +1,33 @@
-/* code to evaluate quadratic equation*/
+/**
+ * @ Author : Sradha S R
+ * @ Create Date : 02-10-2024
+ * @ Description : C Program  to evaluate quadratic equation
+ * @ Revisions :0
+ 
+  
+*/
 
 #include<stdio.h>
-#include<math.h>  //for finding square root 
+#include<math.h>                                                                //for finding square root 
 
-
-void solveQuadraticEqn(float a,float b,float c)
+void main()
 {
-    float discriminant,root1,root2,root3;
+    //Assume the quadratic equation is ax^2+bx+c 
 
-    discriminant=b*b -(4*a*c);  //finding the discriminant
+    float a,b,c,discriminant,root1,root2,root3;
+    printf("enter the co-efficent of x^2 : \n");
+    scanf("%f",&a);
+    printf("enter the co-efficent of x : \n");
+    scanf("%f",&b);
+    printf("enter the co-efficent of 1/ enter the value of 'c' :  \n");
+    scanf("%f",&c);
 
-    //check the value of discriminant
+
+    discriminant=b*b -(4*a*c);                                                  //finding the discriminant using mathematical formula
+
+                                                                                //check the nature of discriminant 
     if (discriminant >0){
-        root1=(-b + sqrt(discriminant))/(2*a);
+        root1=(-b + sqrt(discriminant))/(2*a);                                  //finding the root value using mathematical formula
         root2=(-b - sqrt(discriminant))/(2*a);
         printf("Two distinct real roots : %.2f , %.2f \n",root1,root2);
 
@@ -22,32 +37,11 @@ void solveQuadraticEqn(float a,float b,float c)
         root3=-b/(2*a);
         printf("One real toot: %.2f ",root3);
     }
-    else   //discriminant <0 will give complex roots .
+    else                                                                         //discriminant less than 0 will give complex roots of the form (x+iy)
     {
         float realpart = -b/(2*a);
         float imaginarypart =sqrt(-discriminant)/(2*a);
-        printf("Two complex roots : %.2f +  %.2fi AND %.2f -  %.2fi \n ",realpart,imaginarypart,realpart,imaginarypart);
+        printf("Two complex roots : %.2f + %.2fi AND  %.2f - %.2fi \n ",realpart,imaginarypart,realpart,imaginarypart);
 
-    }
+    }   
 }
-
-
-
-void main()
-{
-    //let the equation is ax^2+bx+c then 
-    float a,b,c;
-    printf("enter the co-efficent of x^2 :");
-    scanf("%f",&a);
-    printf("enter the co-efficent of x :");
-    scanf("%f",&b);
-    printf("enter the co-efficent of 1 :");
-    scanf("%f",&c);
-
-    solveQuadraticEqn(a,b,c);  //call the function to solve equation
-
-    
-}
-
-
-
--- assignment 1/number of 1s.c
+++ assignment 1/number of 1s.c
@@ -1,4 +1,11 @@
-/*code to check number of 1’s in the binary equivalent of a given number.*/
+/**
+ * @ Author : Sradha S R
+ * @ Create Date : 02-10-2024
+ * @ Description: C Program to check number of 1’s in the binary equivalent of a given number.
+ * @ Revisions :0
+ 
+  
+*/
 
 #include<stdio.h>
 
@@ -7,23 +14,24 @@ int countOnes();
 void main()
 {
     int num ,result;
-    printf("enter the number to check: "); //TAKE THE NUMBER TO CKECK FROM USER
+    printf("enter the number to check: ");                          //Take the input from user 
     scanf("%d",&num);
     result=countOnes(num);
     printf("Number of 1's in the given number : %d \n",result);
 
 }
+//Function to find number of 1's in a binary equivalent of a decimal number
 int countOnes(int N)
 
 {
     int count=0;
     while(N>0){
-        if(N & 1)    //For check if LSB is 1 or not
+        if(N & 1)                                                   //Check if LSB is 1 or not
         {      
             count ++;
         }
         
-        N >>=1 ; //Right shifting by 1 bit
+        N >>=1 ;                                                    //Right shifting by 1 bit
     }
     return count;
 
